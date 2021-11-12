8c6f0df55b40de9ce7919fa0be0f1427ed13ad75
Jumeng-ms <465243525@qq.com>
2021-11-12 17:39:02 +0800

    dtCOOKIE.js
    读特cookie

---
+++ dtCOOKIE.js
@@ -0,0 +1,81 @@
+module.exports = {
+    "id": "dt",
+    "name": "����",
+    "keys": ["dturl","dturl2","dturl3","dturl4","dturl5"],
+    "author": "@tom",
+    "settings": [{
+      "id": "dtSuffix",
+      "name": "��ǰ�˺�",
+      "val": "1",
+      "type": "number",
+      "desc": "��ǰץȡck��¼���˺���ţ��磺1��2��3��4"
+    }, {
+      "id": "dtCount",
+      "name": "�˺Ÿ���",
+      "val": "1",
+      "type": "number",
+      "desc": "ָ����������ܼ����˺ţ�����ץȡ���˺����ݸ�������ֵ"
+    }, {
+      "id": "dtXH",
+      "name": "ѭ����ȡck",
+      "val": "0",
+      "type": "number",
+      "desc": "0�رգ�1�򿪣�Ĭ�Ϲر�"
+    }, {
+      "id": "dtTXTX",
+      "name": "txtx",
+      "val": "0",
+      "type": "number",
+      "desc": "0�رգ�1�򿪣�Ĭ�Ϲر�"
+    }, {
+      "id": "dtSC",
+      "name": "sc",
+      "val": "0",
+      "type": "number",
+      "desc": "0�رգ�1�򿪣�Ĭ�Ϲر�"
+    }, {
+      "id": "dtnotifyttt",
+      "name": "���Ϳ���",
+      "val": "1",
+      "type": "number",
+      "desc": "0�رգ�1����,Ĭ��12���Լ�23������"
+    }, {
+      "id": "dtnotifyInterval",
+      "name": "֪ͨ����",
+      "val": "2",
+      "type": "number",
+      "desc": "0�رգ�1Ϊ ����֪ͨ��2Ϊ 12��23 ��֪ͨ��3Ϊ 6��12��18��23 ��֪ͨ"
+    }, {
+      "id": "dtMinutes",
+      "name": "����-֪ͨ ���ӿ���",
+      "val": "10",
+      "type": "number",
+      "desc": "�����Լ�֪ͨ������ʲô���ӶΣ�������0-59,Ĭ��0��10"
+    }],
+    "repo": "https://raw.githubusercontent.com/xl2101200/-/main/dt.js",
+    "icons": ["https://raw.githubusercontent.com/xl2101200/-/main/tom/tom.jpg", "https://raw.githubusercontent.com/xl2101200/-/main/tom/tom.jpg"],
+    "script": "https://raw.githubusercontent.com/xl2101200/-/main/dt.js",
+    "icon": "https://raw.githubusercontent.com/xl2101200/-/main/tom/tom.jpg",
+    "favIcon": "mdi-star-outline",
+    "favIconColor": "grey",
+    "datas": [{
+      "key": "dturl",
+      //https://plus.dutenews.com/api/v2/auth/logindute?app_version=6.3.1&clientid=1&device_id=替换&device_model=xxxx&device_version=iPhone&ip=xxxxxxxxx&memberid=替换&mobile=xxxxxxxxxxx&nickname=xxxxxxxxxxxx&sign=替换&siteid=10001&system_name=android&system_version=xxxxxx&time=1634202874260&type=android
+      "val": ""
+    }, {
+      "key": "dturl2",
+      "val": ""
+    }, {
+      "key": "dturl3",
+      "val": ""
+    }, {
+      "key": "dturl4",
+      "val": ""
+    }, {
+      "key": "dturl5",
+      "val": ""
+    }],
+    "sessions": [],
+    "isLoaded": true
+  }
+  
