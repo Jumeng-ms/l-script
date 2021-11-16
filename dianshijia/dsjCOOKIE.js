d6cc1e688081133c46da3ae4a64bc8b7da7fc7ed
Jumeng-ms <465243525@qq.com>
2021-11-16 11:19:59 +0800

    Update dsjCOOKIE.js

--- dianshijia/dsjCOOKIE.js
+++ dianshijia/dsjCOOKIE.js
@@ -1,89 +1,196 @@
-module.exports = {
-    "id": "dsj",
-    "name": "电视家APP",
-    "keys": ["dsjheader", "dsjtx", "dsjheader2", "dsjtx2", "dsjheader3", "dsjtx3", "dsjheader4", "dsjtx4", "dsjheader5", "dsjtx5", "dsjheader6", "dsjtx6", "dsjheader7", "dsjtx7", "dsjheader8", "dsjtx8", "dsjheader9", "dsjtx9", "dsjheader10", "dsjtx10", "dsjheader11", "dsjtx11", "dsjheader12", "dsjtx12", "dsjheader13", "dsjtx13", "dsjheader14", "dsjtx14", "dsjheader15", "dsjtx15", "dsjheader16", "dsjtx16", "dsjheader17", "dsjtx17", "dsjheader18", "dsjtx18", "dsjheader19", "dsjtx19", "dsjheader20", "dsjtx20"],
-	"author": "@ziye",
-	"settings": [{
-			"id": "dsjSuffix",
-			"name": "当前账号",
-			"val": "1",
-			"type": "number",
-			"desc": "当前抓取ck记录的账号序号，如：1、2、3、"
+module.exports={
+	"id":"dsj",
+	"name":"电视家APP",
+	"keys":["dsjheader","dsjtx","dsjheader2","dsjtx2","dsjheader3","dsjtx3","dsjheader4","dsjtx4","dsjheader5","dsjtx5","dsjheader6","dsjtx6","dsjheader7","dsjtx7","dsjheader8","dsjtx8","dsjheader9","dsjtx9","dsjheader10","dsjtx10","dsjheader11","dsjtx11","dsjheader12","dsjtx12","dsjheader13","dsjtx13","dsjheader14","dsjtx14","dsjheader15","dsjtx15","dsjheader16","dsjtx16","dsjheader17","dsjtx17","dsjheader18","dsjtx18","dsjheader19","dsjtx19","dsjheader20","dsjtx20"],
+	"author":"@ziye",
+	"settings":[
+		{
+			"id":"dsjSuffix",
+			"name":"当前账号",
+			"val":"1",
+			"type":"number",
+			"desc":"当前抓取ck记录的账号序号，如：1、2、3、"
 		},
 		{
-			"id": "dsjCount",
-			"name": "账号个数",
-			"val": "1",
-			"type": "number",
-			"desc": "指定任务最多跑几个账号，根据抓取的账号数据个数来设值"
+			"id":"dsjCount",
+			"name":"账号个数",
+			"val":"10",
+			"type":"number",
+			"desc":"指定任务最多跑几个账号，根据抓取的账号数据个数来设值"
 		},
 		{
-			"id": "dsjXH",
-			"name": "循环获取CK",
-			"val": "0",
-			"type": "number",
-			"desc": "0关闭 1开启,默认关闭"
+			"id":"dsjXH",
+			"name":"循环获取CK",
+			"val":"0",
+			"type":"number",
+			"desc":"0关闭 1开启,默认关闭"
 		},
 		{
-			"id": "dsjZDTX",
-			"name": "自动提现",
-			"val": "0",
-			"type": "number",
-			"desc": "可设置0,1,5,10,20,25,30,50,99  0不提现 1随机提现 99由上到下所有提现 其他固定提现"
+			"id":"dsjZDTX",
+			"name":"自动提现",
+			"val":"10",
+			"type":"number",
+			"desc":"可设置0,1,5,10,20,25,30,50,99  0不提现 1随机提现 99由上到下所有提现 其他固定提现"
 		},
 		{
-			"id": "dsjXYZ",
-			"name": "执行概率",
-			"val": "100",
-			"type": "number",
-			"desc": "0不执行 可设置0-100,默认百分百"
+			"id":"dsjXYZ",
+			"name":"执行概率",
+			"val":"100",
+			"type":"number",
+			"desc":"0不执行 可设置0-100,默认百分百"
 		},
 		{
-			"id": "dsjTXTX",
-			"name": "余额提醒",
-			"val": "0",
-			"type": "number",
-			"desc": "0不提醒 可设置0,5,10,20,25,30,50,100"
+			"id":"dsjTXTX",
+			"name":"余额提醒",
+			"val":"0",
+			"type":"number",
+			"desc":"0不提醒 可设置0,5,10,20,25,30,50,100"
 		},
 		{
-			"id": "dsjnotifyttt",
-			"name": "推送控制",
-			"val": "1",
-			"type": "number",
-			"desc": "0关闭，1推送,默认12点以及23点推送"
+			"id":"dsjnotifyttt",
+			"name":"推送控制",
+			"val":"1",
+			"type":"number",
+			"desc":"0关闭，1推送,默认12点以及23点推送"
 		},
 		{
-			"id": "dsjnotifyInterval",
-			"name": "通知控制",
-			"val": "2",
-			"type": "number",
-			"desc": "0关闭，1为 所有通知，2为 12，23 点通知，3为 6，12，18，23 点通知 "
+			"id":"dsjnotifyInterval",
+			"name":"通知控制",
+			"val":"2",
+			"type":"number",
+			"desc":"0关闭，1为 所有通知，2为 12，23 点通知，3为 6，12，18，23 点通知 "
 		},
 		{
-			"id": "dsjMinutes",
-			"name": "推送-通知 分钟控制",
-			"val": "10",
-			"type": "number",
-			"desc": "推送以及通知控制在什么分钟段，可设置0-59,默认0到10"
+			"id":"dsjMinutes",
+			"name":"推送-通知 分钟控制",
+			"val":"10","type":"number","desc":"推送以及通知控制在什么分钟段，可设置0-59,默认0到10"
 		}
 	],
-	"repo": "https://cdn.jsdelivr.net/gh/ziye888/JavaScript@main/Task/dsj.js",
-	"icons": ["https://cdn.jsdelivr.net/gh/ziye888/JavaScript@main/Task/dsj.png", "https://cdn.jsdelivr.net/gh/ziye888/JavaScript@main/Task/dsj.png"],
-	"script": "https://cdn.jsdelivr.net/gh/ziye888/JavaScript@main/Task/dsj.js",
-	"datas": [{
-	  "key": "dsjheader",
-	  "val": ""
-	}, 
-	{
-	  "key": "dsjheader2",
-	  "val": ""
-	}, 
-	{
-	  "key": "dsjheader3",
-	  "val": ""
-	}
+	"repo":"https://cdn.jsdelivr.net/gh/ziye888/JavaScript@main/Task/dsj.js",
+	"icons":["https://cdn.jsdelivr.net/gh/ziye888/JavaScript@main/Task/dsj.png","https://cdn.jsdelivr.net/gh/ziye888/JavaScript@main/Task/dsj.png"],
+	"script":"https://cdn.jsdelivr.net/gh/ziye888/JavaScript@main/Task/dsj.js","icon":"https://cdn.jsdelivr.net/gh/ziye888/JavaScript@main/Task/dsj.png",
+	"favIcon":"mdi-star-outline","favIconColor":"grey",
+	"datas":[
+		{
+		"key":"dsjheader",
+		"val":"8f3ad4fa0dd9269c073bdd23c980864f&WTJVd05USTVPVFJqTnpFd05HSTRaR1JoWVRoaU9URXpaVEU0Wm1Nek1Uaz18MTYzMzkzNDE1ODg2MzY2NzMyMXw5MmJjOTcwZjFiMTcyMjBhMzgyY2U3N2JhYTNjN2UzNTE0ZDczNDMw"
+		},
+		{
+		"key":"dsjtx",
+		"val":""
+		},
+		{
+		"key":"dsjheader2",
+		"val":"ebc02e88939a4fe5be61f5ff1419b91b&T1RWaE1UaGxNVFkzTmpjd1pUVTRNVFJoWlRGak56ZzNNemc1T0RVMU16WT18MTYzNDYzNTIzODMzMTkyNDU0M3w3Y2NjYWZkZjhiYmNiMGFjMWZhNGNmMmY0MTc4ZTI2NGZmN2E0NzVk"
+		},
+		{
+		"key":"dsjtx2",
+		"val":""
+		},
+		{
+		"key":"dsjheader3",
+		"val":"3ca48e03c3cffdbc950e78df2ede4dae&T1RjMk56aGpOMkUwTkdZM09UQTFPR1JsWlRFM05qbGpNR1JtTVdWbU9UWT18MTYzNjc4MDY1NTc4Nzc5NTQ3N3xmNmExYzZkYmMyMDk3OWZhMzZjMDUwOTFjOGMzMDgwMjU1MWQ2Y2M0"
+		},
+		{
+		"key":"dsjtx3",
+		"val":""
+		},
+		{
+		"key":"dsjheader4",
+		"val":""
+		},
+		{
+		"key":"dsjtx4",
+		"val":""
+		},
+		{
+		"key":"dsjheader5",
+		"val":""
+		},
+		{
+		"key":"dsjtx5",
+		"val":""
+		},
+		{
+		"key":"dsjheader6",
+		"val":""
+		},
+		{
+		"key":"dsjtx6",
+		"val":""
+		},
+		{
+		"key":"dsjheader7",
+		"val":""
+		},
+		{
+		"key":"dsjtx7",
+		"val":""
+		},
+		{
+		"key":"dsjheader8",
+		"val":""
+		},
+		{
+		"key":"dsjtx8",
+		"val":""
+		},
+		{
+		"key":"dsjheader9",
+		"val":""
+		},
+		{
+		"key":"dsjtx9",
+		"val":""
+		},
+		{
+		"key":"dsjheader10",
+		"val":""
+		},
+		{"key":"dsjtx10",
+		"val":""
+		},
+		{
+		"key":"dsjheader11",
+		"val":""
+		},
+		{
+		"key":"dsjtx11",
+		"val":""
+		},
+		{
+		"key":"dsjheader12",
+		"val":""
+		},
+		{
+		"key":"dsjtx12",
+		"val":""
+		},
+		{
+		"key":"dsjheader13",
+		"val":""
+		},
+		{
+		"key":"dsjtx13",
+		"val":""
+		},
+		{
+		"key":"dsjheader14",
+		"val":""
+		},
+		{
+		"key":"dsjtx14",
+		"val":""
+		},
+		{
+		"key":"dsjheader15",
+		"val":""
+		},
+		{
+		"key":"dsjtx15",
+		"val":""
+		}
 	],
-	"sessions": [],
-	"isLoaded": true
+	"sessions":[],
+	"isLoaded":true
 }
-  
